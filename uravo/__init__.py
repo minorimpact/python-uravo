ce4bf49a4b34e6f651ea09ebadf1a8691f92de96
Patrick Gillan <patrick@minorimpact.com>
2021-09-25 13:19:07 -0400

    Updated documentation, minor cleanup.

--- uravo/__init__.py
+++ uravo/__init__.py
@@ -2,8 +2,12 @@
 
 from uravo.uravo import Uravo
 
+__version__ = "0.0.3"
+
 uravo = Uravo()
+
 def connect():
     return uravo
 
-
+def main():
+    uravo.main()
--- uravo/uravo.py
+++ uravo/uravo.py
@@ -13,8 +13,7 @@ class Uravo():
     def __init__(self):
         self.config = uravo.config.config
         self.db = None
-        #self.db = MySQLdb.connect(host=self.config["agent"]["outpost_server"],user=self.config["agent"]["db_user"],passwd=self.config["agent"]["db_password"],db="uravo", port=int(self.config["agent"]["outpost_db_port"]), autocommit=True)
-        self.connect();
+        self.connect()
     
     def alert(self, server_id = None, AlertGroup = None, Severity = None, Summary = None, AlertKey = None, AdditionalInfo = '', Recurring = 0, Timeout = None, Agent = None):
         self.event(server_id = server_id, AlertGroup = AlertGroup, Severity = Severity, Summary = Summary, AlertKey = AlertKey, AdditionalInfo = AdditionalInfo, Recurring = Recurring, Timeout = Timeout, Agent = Agent)
@@ -38,12 +37,7 @@ class Uravo():
             server_id = re.sub(r"^([^.]+).*$", r"\1", socket.gethostname())
         if (server_id is None or AlertGroup is None or Severity is None or Summary is None): return
 
-        # TODO: Add Server object.
-        #server = self.getServer(server_id)
-        #if (server is None): return
 
-        #cluster_id = server.cluster_id()
-        cluster_id = 'default'
         if AlertKey is None and AlertGroup is not None:
             AlertKey = AlertGroup
         if (Timeout is None):
@@ -51,21 +45,12 @@ class Uravo():
         elif Timeout < 1400:
             Timeout = int(time.time()) + (Timeout * 60)
 
-        #print "  $data->{server_id}: $data->{Severity} - $data->{Summary}\n" if ($uravo->{options}->{verbose});
         if (Severity == 'red'): Severity = 4
         elif (Severity == 'orange'): Severity = 4
         elif (Severity == 'yellow'): Severity = 3 
         elif (Severity == 'blue'): Severity = 2
         elif (Severity == 'gray'): Severity = 1 
         elif (Severity == 'green'): Severity = 0 
-        #foreach my $key (keys %$data) {
-        #    my $value = $key;
-        #    $value = substr($value, 0, 16384);
-        #    if ($value && $value < .001 && $value=~/^[0-9.e-]+$/) {
-        #        $value = sprintf("%.8f", $value);
-        #    }
-        #    $key = $value;
-        #}
 
         if (Agent is None):
             Agent = f"{server_id}:{os.path.basename(__file__)}"
@@ -79,12 +64,6 @@ class Uravo():
         if (Recurring > 0):
             sql = "UPDATE alert SET Severity=0 WHERE AlertGroup='timeout' AND AlertKey=%s AND server_id=%s"
             c.execute(sql, (AlertGroup, server_id))
-
-        #my $alerts = $self->getCache("active_alerts");
-        #if (!$alerts) {
-        #    $alerts = $self->{db}->selectall_hashref("SELECT Identifier, Severity FROM alert WHERE Severity > 0", "Identifier");
-        #    $self->setCache("active_alerts", $alerts);
-        #}
 
         if Severity == 0 and Identifier is None:
             return
